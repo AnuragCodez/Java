ce57f0517255f7e9568030d8b86f65b42ac2d4dc
Anurag Kumar Maurya <anuragkumarmaurya000@gmail.com>
2023-04-30 12:27:24 +0530

    new

---
+++ function.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class function {
+    
+    public static void main(String args[])
+    {
+     String dashdash= dash();
+
+System.out.println(dashdash);
+    }
+      static String dash()
+      {
+        String greeting = "How are you";
+
+        return greeting;
+
+      }
+      }
